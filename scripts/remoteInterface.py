01a6119f18189e83ab761f09142222def1c17f91
frknilisu <frkn.ilisu@gmail.com>
2020-06-23 12:45:12 +0300

    use tcp socket server in remoteInterface

--- scripts/remoteInterface.py
+++ scripts/remoteInterface.py
@@ -3,14 +3,34 @@
 import rospy
 from std_msgs.msg import Int64
 from motion_box.msg import TimelapseMsg, CmdStepMsg
+from networks import Server, Client
 
+def parseData(data):
+    rospy.logdebug(data)
+    #angle = data.get("angle", 180)
+    msg = TimelapseMsg()
+    return msg
 
 if __name__ == "__main__":
-    rospy.init_node('remoteInterface')
+    try:
+        rospy.init_node('remoteInterface', log_level=rospy.DEBUG)
+        r = rospy.Rate(10) # 10Hz
 
-    pub_timelapse = rospy.Publisher(
-        "/cmd_timelapse", TimelapseMsg, queue_size=10)
-    pub_simplemove = rospy.Publisher(
-        "/cmd_simplemove", CmdStepMsg, queue_size=10)
+        pub_timelapse = rospy.Publisher(
+            "/cmd_timelapse", TimelapseMsg, queue_size=10)
+        pub_simplemove = rospy.Publisher(
+            "/cmd_simplemove", CmdStepMsg, queue_size=10)
 
-    rospy.spin()
+        server = Server(host='0.0.0.0', port=65432)
+
+        while not rospy.is_shutdown():
+            server.accept()
+            json_data = server.recv()
+            msg = parseData(json_data)
+            if type(msg) == TimelapseMsg:
+                pub_timelapse.publish(msg)
+            elif type(msg) == CmdStepMsg:
+                pub_simplemove.publish(msg)
+            r.sleep()
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
