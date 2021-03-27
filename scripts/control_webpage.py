540038b78a1494f97333c66a9dc4cec9fb76d3e6
frknilisu <frkn.ilisu@gmail.com>
2021-03-27 05:53:56 +0300

    flask server with angular page publishing ros message

--- scripts/control_webpage.py
+++ scripts/control_webpage.py
@@ -1,33 +1,56 @@
+#! /usr/bin/python3
+
+import rospy
+import json
+import signal
 import sys
-print(sys.version)
-import time
 
-import timelapse
+from std_msgs.msg import String
 
-from flask import Flask, render_template, render_template_string, request, redirect
-app = Flask(__name__)
+from threading import Thread
+from flask import Flask, render_template, render_template_string, request, redirect, jsonify
 
+Thread(target=lambda: rospy.init_node('remote_listener', disable_signals=True)).start()
+pub = rospy.Publisher('mission', String, queue_size=1)
 
-
-
+app = Flask(__name__, template_folder='../templates')
 
 @app.route("/")
 def index():
-    return render_template("timelapse.html")
+    return render_template("angular_control.html")
 
 @app.route("/timelapse", methods=["POST"])
-def f():
+def timelapse():
     d = request.form.to_dict()
-    angle = int(d["angle"])
+
+    record_time = float(d["record_time"])
+    d["record_time"] = record_time
+    play_time = float(d["play_time"])
+    d["play_time"] = play_time
     fps = int(d["fps"])
-    record_time = int(d["record_time"])
-    play_time = int(d["play_time"])
+    d["fps"] = fps
+    no_of_photos = int(d["no_of_photos"])
+    d["no_of_photos"] = no_of_photos
+    interval_time = float(d["interval_time"])
+    d["interval_time"] = interval_time
+    video_speed = int(d["video_speed"])
+    d["video_speed"] = video_speed
+    degree = int(d["degree"])
+    d["degree"] = degree
     direction = bool(d["direction"])
-    timelapse.start(record_time, play_time, fps, angle, direction)
+    d["direction"] = direction
+
+    d["command"] = "debug"
+    pub.publish(json.dumps(d))
 
     return redirect('/')
 
+def signal_handler(signal, frame):
+    rospy.signal_shutdown("end")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT,signal_handler)
 
 # Run the app on the local development server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- scripts/missionController.py
+++ scripts/missionController.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import rospy
+import json
 
 from std_msgs.msg import String
 
@@ -9,23 +10,29 @@ from missions import VideoTimelapse, PhotoTimelapse
 controller = None
 
 def mission_photo_timelapse_cb(msg):
+    global controller
+    
     rospy.loginfo("mission_photo_timelapse_cb()..")
-    if msg['command'] == "start":
+    data = json.loads(msg.data)
+    print(data)
+    if data['command'] == "start":
         if controller is None or not controller.is_running:
             controller = PhotoTimelapse(msg)
         else:
             rospy.logwarn("Controller is running already")
-    elif msg['command'] == "stop":
+    elif data['command'] == "stop":
         if not controller is None and controller.is_running:
             controller.stop()
         else:
             rospy.logwarn("Controller is not running")
+    else:
+        rospy.logwarn("Unknown command")
 
 if __name__ == "__main__":
     rospy.init_node('missionController', log_level=rospy.DEBUG)
     r = rospy.Rate(10) # 10Hz
 
-    rospy.Subscriber("mission/photo_timelapse", String, mission_photo_timelapse_cb)
+    rospy.Subscriber("mission", String, mission_photo_timelapse_cb)
 
     while not rospy.is_shutdown():
         if not controller is None and controller.state == "ready":
