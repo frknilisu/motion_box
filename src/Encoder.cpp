2c8c85bd5f12719ef48539426e054b0b9b5232e1
frknilisu <frkn.ilisu@gmail.com>
2021-04-21 17:46:09 +0300

    add encoder cpp node with using AS5600 class

---
+++ src/Encoder.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include "AS5600.h"
+
+int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "encoder_node");
+    ros::NodeHandle nh;
+    ROS_INFO("Hello, World!");
+
+    AMS_5600 ams5600;
+    ams5600.setup();
+    uint16_t prev_raw_angle = 0;
+    uint16_t curr_raw_angle = 0;
+    while(ros::ok()) {
+        curr_raw_angle = ams5600.getRawAngle();
+        if(prev_raw_angle != curr_raw_angle)
+            std::cout << "Raw Angle: " << curr_raw_angle << std::endl;
+        prev_raw_angle = curr_raw_angle;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        ros::spinOnce();
+    }
+}
