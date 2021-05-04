dd16d8aa0805715f2b6d63dbf39d85357d337a67
frknilisu <frkn.ilisu@gmail.com>
2021-05-04 16:16:19 +0300

    try gtest unittest

--- test/AS5600_test.cpp
+++ test/AS5600_test.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <wiringPiI2C.h>
+#include <cmath>
 
-#include "../src/AS5600.h"
+//#include "../src/AS5600.h"
 
-int main (int argc, char **argv)
+#include <gtest/gtest.h>
+
+class AS5600Test : public ::testing::Test
 {
-    AS5600 encoder;
-    encoder.setup();
-    uint16_t start_raw_angle = 0;
-    uint16_t prev_raw_angle = 0;
-    uint16_t curr_raw_angle = 0;
-    curr_raw_angle = encoder.getRawAngle();
-    std::cout << curr_raw_angle << std::endl;
-    std::cout << encoder.setStartPosition(curr_raw_angle) << std::endl;
-    //std::cout << encoder.setEndPosition(180) << std::endl;
-    //std::cout << encoder.setMaxAngle(135) << std::endl;
-    start_raw_angle = curr_raw_angle;
-    std::cout << "Start Position: " << start_raw_angle << std::endl;
-    std::cout << "End Position: " << encoder.getEndPosition() << std::endl;
-    std::cout << "Max Angle: " << encoder.getMaxAngle() << std::endl;
-    while(ros::ok()) {
-        if(encoder.isMagnetDetected()) {
-            curr_raw_angle = encoder.getRawAngle();
-            if(prev_raw_angle != curr_raw_angle) {
-                std::cout << "Raw Angle: " << curr_raw_angle << std::endl;
-                std::cout << "Scaled Angle: " << encoder.getScaledAngle() << std::endl;
-                std::cout << "Diff: " << curr_raw_angle - start_raw_angle << std::endl;
-            }
-            prev_raw_angle = curr_raw_angle;
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        ros::spinOnce();
+public:
+
+    AS5600Test()
+    {
+        std::cout << "AS5600Test()" << std::endl;
+        //AS5600 encoder(0x40);
+        //encoder.setup();
+        this->_i2cAddress = 0x40;
     }
 
-    return 0;
+    virtual void SetUp()
+    {
+        std::cout << "SetUp()" << std::endl;
+    }
+
+    virtual void TearDown()
+    {
+        std::cout << "TearDown()" << std::endl;
+    }
+
+    ~AS5600Test()
+    {
+        std::cout << "~AS5600Test()" << std::endl;
+    }
+
+    uint16_t getStartPosition() const { return this->_startPosition; }
+    void setStartPosition(uint16_t startPosition) { this->_startPosition = startPosition; }
+    uint16_t getEndPosition() const { return this->_endPosition; }
+    void setEndPosition(uint16_t endPosition) { this->_endPosition = endPosition; }
+
+    uint16_t getAddress() const { return this->_i2cAddress; }
+
+private:
+    uint16_t _startPosition;
+    uint16_t _endPosition;
+    uint16_t _i2cAddress;
+
+};
+
+TEST_F(AS5600Test, DefaultConstructor)
+{
+    EXPECT_EQ(0x40, getAddress());
+}
+
+
+TEST_F(AS5600Test, SetStartPosition) {
+    const uint16_t startPosition = 0x14;
+    setStartPosition(startPosition);
+    //encoder.setStartPosition(startPosition);
+    //EXPECT_EQ(startPosition, encoder.getStartPosition());
+    EXPECT_EQ(startPosition, getStartPosition());
+}
+
+TEST_F(AS5600Test, SetEndPosition) {
+    const uint16_t endPosition = 0x0c80;
+    setEndPosition(endPosition);
+    //encoder.setEndPosition(endPosition);
+    //EXPECT_EQ(endPosition, encoder.getEndPosition());
+    EXPECT_EQ(endPosition, getEndPosition());
+}
+
+TEST(SquareRootTest, PositiveNos) { 
+    EXPECT_DOUBLE_EQ(18.0, sqrt(324.0));
+    EXPECT_DOUBLE_EQ(25.4, sqrt(645.16));
+    EXPECT_DOUBLE_EQ(50.332, sqrt(2533.310224));
 }
