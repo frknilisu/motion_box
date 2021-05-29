352a678d008e4b4ec8bee81c75371d959f21e438
frknilisu <frkn.ilisu@gmail.com>
2021-05-29 13:21:50 +0300

    publish joystick cmd_vel

--- scripts/joystick.py
+++ scripts/joystick.py
@@ -2,10 +2,11 @@
 
 import rospy
 import time
-
-from std_msgs.msg import Int32MultiArray
-
+import json
 import spidev
+
+from std_msgs.msg import String, Int32MultiArray
+
 
 axis_thresholds = (0, 500, 523, 1023)
 speed_range = (5, 50)
@@ -38,7 +39,6 @@ class MCP3008:
 
         return (vrx_pos, vry_pos, swt_val)
 
-
 #  Prominent Arduino map function :)
 def map_val(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
@@ -53,7 +53,8 @@ def checkJoyStatDiff(curr, prev):
 
 if __name__ == "__main__":
     rospy.init_node('joystick', log_level=rospy.DEBUG)
-    pub_joy = rospy.Publisher("joystick_status", Int32MultiArray, queue_size=10)
+    pub_joy_stat = rospy.Publisher("joystick/status", Int32MultiArray, queue_size=10)
+    pub_joy_cmd_vel = rospy.Publisher("joystick/cmd_vel", String, queue_size=10)
 
     joystick = MCP3008()
 
@@ -69,12 +70,6 @@ if __name__ == "__main__":
 
         # output
         rospy.loginfo("VRx: {}  VRy: {}  SW: {}".format(*curr_joy_stat))
-
-        if checkJoyStatDiff(curr_joy_stat, prev_joy_stat):
-            joy_msg = Int32MultiArray()
-            joy_msg.data = curr_joy_stat
-            pub_joy.publish(joy_msg)
-
 
         if curr_joy_stat[0] > 523:
             # map the speed between 5 and 500 rpm
@@ -95,6 +90,18 @@ if __name__ == "__main__":
         sw_pressed = (curr_joy_stat[2] < 10)
 
         rospy.logdebug("vx: {}  vy: {}  sw: {}".format(vx, vy, sw_pressed))
+
+        data = json.dumps({
+            'vx': vx, 
+            'vy': vy, 
+            'sw': sw_pressed
+        })
+        pub_joy_cmd_vel.publish(data)
+
+        if checkJoyStatDiff(curr_joy_stat, prev_joy_stat):
+            joy_msg = Int32MultiArray()
+            joy_msg.data = curr_joy_stat
+            pub_joy_stat.publish(joy_msg)
 
         prev_joy_stat = curr_joy_stat
 
