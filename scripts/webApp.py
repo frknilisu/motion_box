30bb1fb1cf3fb9702b9529d733d89ac31267a059
frknilisu <frkn.ilisu@gmail.com>
2021-05-29 16:34:57 +0300

    add joy post by web app

--- scripts/webApp.py
+++ scripts/webApp.py
@@ -6,12 +6,19 @@ import signal
 import sys
 
 from std_msgs.msg import String
+from motion_box.srv import StringTrigger, StringTriggerRequest
 
 from threading import Thread
-from flask import Flask, render_template, render_template_string, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, jsonify
 
 Thread(target=lambda: rospy.init_node('remote_listener', disable_signals=True)).start()
-pub = rospy.Publisher('mission', String, queue_size=1)
+
+pub_mission = rospy.Publisher('mission', String, queue_size=1)
+
+rospy.wait_for_service('motor/drive/action')
+motor_drive_action_srv = rospy.ServiceProxy('motor/drive/action', StringTrigger)
+
+pub_joy_cmd_vel = rospy.Publisher('joystick/cmd_vel', String, queue_size=1)
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -22,27 +29,79 @@ def index():
 @app.route("/timelapse", methods=["POST"])
 def timelapse():
     d = request.form.to_dict()
+    print(d)
 
     record_time = float(d["record_time"])
-    d["record_time"] = record_time
     play_time = float(d["play_time"])
-    d["play_time"] = play_time
     fps = int(d["fps"])
-    d["fps"] = fps
     no_of_photos = int(d["no_of_photos"])
-    d["no_of_photos"] = no_of_photos
     interval_time = float(d["interval_time"])
-    d["interval_time"] = interval_time
     video_speed = int(d["video_speed"])
-    d["video_speed"] = video_speed
     degree = int(d["degree"])
-    d["degree"] = degree
     direction = True if d["direction"] == "True" else False
-    d["direction"] = direction
 
-    d["command"] = "start"
-    pub.publish(json.dumps(d))
+    data = json.dumps({
+        'command': 'start', 
+        'record_time': record_time, 
+        'play_time': play_time, 
+        'fps': fps, 
+        'no_of_photos': no_of_photos, 
+        'interval_time': interval_time, 
+        'video_speed': video_speed, 
+        'degree': degree, 
+        'direction': direction
+    })
+    print(data)
+    pub_mission.publish(data)
 
+    return redirect('/')
+
+@app.route("/test", methods=["POST"])
+def test():
+    d = request.form.to_dict()
+    print(d)
+
+    step_number = int(d["step_number"])
+    direction = True if d["direction"] == "True" else False
+    step_type = str(d["step_types"]).split()[0]
+    speed = float(d["speed"])
+    
+    data = json.dumps({
+        'command': 'move', 
+        'steps': step_number, 
+        'direction': direction,  
+        'step_type': step_type, 
+        'speed': speed
+    })
+    print(data)
+    motor_drive_action_msg = StringTriggerRequest(data=data)
+    result = motor_drive_action_srv(motor_drive_action_msg)
+    print(result)
+
+    return redirect('/')
+
+@app.route("/joy", methods=["POST"])
+def joy():
+    d = request.form.to_dict()
+    print(d)
+
+    vx = int(d["vx"])
+    vy = int(d["vy"])
+    step_number = int(d["step_number"])
+    direction = True if d["direction"] == "True" else False
+    step_type = str(d["step_types"]).split()[0]
+    speed = float(d["speed"])
+    
+    data = json.dumps({
+        'command': 'move', 
+        'steps': step_number, 
+        'direction': direction,  
+        'step_type': step_type, 
+        'speed': speed
+    })
+    print(data)
+    pub_joy_cmd_vel.publish(data)
+    
     return redirect('/')
 
 def signal_handler(signal, frame):
